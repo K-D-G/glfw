e55cf156db8d1a662a0754c3a6b1da0fba1942de
K-D-G <33807054+K-D-G@users.noreply.github.com>
2020-10-17 21:51:00 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,68 +1,102 @@
 project "GLFW"
 	kind "StaticLib"
 	language "C"
+	architecture "x86_64"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir "../bin/%{cfg.buildcfg}"
+	objdir "../obj/%{cfg.buildcfg}"
 
-	files
-	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+	includedirs {"glfw/include/"}
+
+	files{
+--		"glfw/include/GLFW/glfw3.h",
+--		"glfw/include/GLFW/glfw3native.h",
+		"glfw/src/glfw_config.h",
+		"glfw/src/context.c",
+		"glfw/src/init.c",
+		"glfw/src/input.c",
+		"glfw/src/monitor.c",
+		"glfw/src/vulkan.c",
+		"glfw/src/window.c"	
 	}
+
 	filter "system:linux"
 		pic "On"
-
 		systemversion "latest"
 		staticruntime "On"
-
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+		files{
+			"glfw/src/x11_init.c",
+			"glfw/src/x11_monitor.c",
+			"glfw/src/x11_window.c",
+			"glfw/src/xkb_unicode.c",
+			"glfw/src/posix_time.c",
+			"glfw/src/posix_thread.c",
+			"glfw/src/glx_context.c",
+			"glfw/src/egl_context.c",
+			"glfw/src/osmesa_context.c",
+			"glfw/src/linux_joystick.c"	
 		}
 
-		defines
-		{
+		defines{
 			"_GLFW_X11"
 		}
-
 	filter "system:windows"
 		systemversion "latest"
 		staticruntime "On"
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
+		files{
+			"glfw/src/win32_init.c",
+			"glfw/src/win32_joystick.c",
+			"glfw/src/win32_monitor.c",
+			"glfw/src/win32_time.c",
+			"glfw/src/win32_thread.c",
+			"glfw/src/win32_window.c",
+			"glfw/src/wgl_context.c",
+			"glfw/src/egl_context.c",
+			"glfw/src/osmesa_context.c"
 		}
 
-		defines 
-		{ 
+		defines{
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+	filter "system:macosx"
+		language "objective-c"
+		systemversion "latest"
+		staticruntime "On"
+
+		files{
+			"glfw/src/cocoa_platform.h",
+			"glfw/src/cocoa_joystick.h",
+			"glfw/src/posix_thread.h",
+			"glfw/src/nsgl_context.h",
+			"glfw/src/egl_context.h",
+			"glfw/src/osmesa_context.h",
+			"glfw/src/cocoa_init.m",
+			"glfw/src/cocoa_joystick.m", 
+			"glfw/src/cocoa_monitor.m",
+			"glfw/src/cocoa_window.m",
+			"glfw/src/cocoa_time.c",
+			"glfw/src/posix_thread.c",
+			"glfw/src/nsgl_context.m",
+			"glfw/src/egl_context.c",
+			"glfw/src/osmesa_context.c"
+		}
+
+		links{
+	        "X11.6",                 
+	        "pthread",               
+	        "Xrandr.2",              
+	        "Xi.6",                  
+	        "Cocoa.framework",       
+	        "IOKit.framework",       
+	        "CoreVideo.framework",   
+	        "OpenGL.framework",
+		}
+
+		defines{
+			"_GLFW_COCOA"
 		}
 
 	filter "configurations:Debug"
@@ -72,3 +106,4 @@ project "GLFW"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+
